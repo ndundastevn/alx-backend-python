696fc16502077968284e6c530c4bf17889d720a3
ndundastevn <ndundastevn@gmail.com>
2023-12-07 11:01:13 -0800

    no color

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 0: Basic varable annotations - add
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''Add two floating-point numbers.
+    '''
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 2:Basici ariable  annotations - floor
+'''
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return int(a)
